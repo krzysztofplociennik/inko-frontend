93cd87ff8ad1a035aec35a1ee29adbeac1aad8ee
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2024-04-17 07:36:08 +0200

    Created placeholders (header, footer).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from '../app/posts/posts.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
