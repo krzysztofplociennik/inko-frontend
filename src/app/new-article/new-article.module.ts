c71d69df92cf77a4088f9c9a08e027462851cf1b
kplociennik <krzysztofplociennik6@gmail.com>
2024-07-08 19:27:14 +0200

    Added missing changes.

--- src/app/new-article/new-article.module.ts
+++ src/app/new-article/new-article.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateArticleService } from './service/create-article.service';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
   ],
   imports: [
     CommonModule,
-    CKEditorModule,
   ],
   exports: [],
   providers: [
