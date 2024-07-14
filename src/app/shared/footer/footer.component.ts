895d13a425b4064c270cf863dc3a46e2d974d237
kplociennik <krzysztofplociennik6@gmail.com>
2024-07-14 20:23:15 +0200

    Fixed footer at the search articles view.

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class FooterComponent {
 
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class HeaderComponent {
   search_articles: string|any[]|null|undefined = 'search';
