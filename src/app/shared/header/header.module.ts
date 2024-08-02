d7d74f5f7a3031afe295d352096e9c5d057c7f9a
kplociennik <krzysztofplociennik6@gmail.com>
2024-08-02 20:35:19 +0200

    Deleted header-button component.

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderButtonComponent } from '../header-button/header-button.component';
 
 @NgModule({
-  declarations: [
-    HeaderButtonComponent
-  ],
   imports: [
     CommonModule
   ],
-  exports: [
-    HeaderButtonComponent
-  ]
 })
 export class HeaderModule { }
