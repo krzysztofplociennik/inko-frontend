b894b16082f620f3da6d36aac8fae2c948d3c865
kplociennik <krzysztofplociennik6@gmail.com>
2024-08-06 20:47:05 +0200

    Added some styling at the all articles item's view.

--- src/app/articles/all-articles-item/all-articles-item.component.ts
+++ src/app/articles/all-articles-item/all-articles-item.component.ts
@@ -12,15 +12,14 @@ export class AllArticlesItemComponent {
   @Input() isHovered: boolean = false;
 
   getCurrentStyle(): { [klass: string]: any; } {
-    
-    if(this.isHovered) {
-      return {
-        'background':'#e6eef5'
-      };
-    }
     return {
-      'background':'white'
+      'height': '130px',
+      'background': this.getBackgroundColor(),
     };
+  }
+
+  getBackgroundColor(): string { 
+    return this.isHovered? '#e6eef5' : 'white';
   }
 
 }
