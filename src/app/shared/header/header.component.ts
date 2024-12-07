452dcbc1a0f739b0afcb344e52dd821458bed0df
kplociennik <krzysztofplociennik6@gmail.com>
2024-12-07 14:24:46 +0100

    Added icons to the theme switch button.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,18 +5,21 @@ import { ThemeService } from '../services/theme.service';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class HeaderComponent {
-  search_articles: string|any[]|null|undefined = 'search';
-  home: string|any[]|null|undefined = 'home';
-  articles: string|any[]|null|undefined = 'articles';
-  about: string|any[]|null|undefined = 'about';
+  search_articles: string | any[] | null | undefined = 'search';
+  home: string | any[] | null | undefined = 'home';
+  articles: string | any[] | null | undefined = 'articles';
+  about: string | any[] | null | undefined = 'about';
 
   constructor(private themeService: ThemeService) {}
 
-
   toggleTheme(): void {
     this.themeService.toggleDarkMode();
+  }
+
+  isDarkMode(): boolean {
+    return this.themeService.isDarkMode();
   }
 }
