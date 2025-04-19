3c832a7b752c236dafc21957148515870379cded
kplociennik <krzysztofplociennik6@gmail.com>
2025-04-19 19:41:36 +0200

    Improved login functionality in terms of session activity (checking expiration and refreshing).

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,27 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, catchError, Observable, of, tap, throwError } from 'rxjs';
 import { getBaseUrl } from '../utils/urlUtils';
+import { MessageService } from 'primeng/api';
+import { JwtUtils } from '../utils/jwtUtils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = '';
-
-  private loginStateSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
+  private tokenKey = 'token';
+  private loginStateSubject = new BehaviorSubject<boolean>(false);
+  private refreshInProgress = false;
 
   loginState$ = this.loginStateSubject.asObservable();
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {
     this.apiUrl = getBaseUrl();
+    this.checkAuthStatus();
+  }
+
+  private checkAuthStatus(): void {
+    const isValid = this.isTokenValid();
+    this.loginStateSubject.next(isValid);
+    
+    if (isValid && this.shouldRefreshToken()) {
+      this.performTokenRefresh();
+    }
   }
 
   login(credentials: { username: string; password: string }): Observable<AuthResponse> {
-
     return this.http.post<AuthResponse>(`${this.apiUrl}/api/auth/login`, credentials).pipe(
       tap(response => {
-        localStorage.setItem('jwt', response.token);
+        if (response && response.token) {
+          this.setToken(response.token);
+        }
         return response;
       }),
       catchError(error => {
@@ -36,25 +53,105 @@ export class AuthService {
   }
 
   setToken(token: string): void {
-    localStorage.setItem('token', token);
+    localStorage.setItem(this.tokenKey, token);
     this.updateLoginState(true);
   }
 
   getToken(): string | null {
-    return localStorage.getItem('token');
+    return localStorage.getItem(this.tokenKey);
   }
 
   clearToken(): void {
-    localStorage.removeItem('token');
+    localStorage.removeItem(this.tokenKey);
     this.updateLoginState(false);
   }
 
   isLoggedIn(): boolean {
-    return this.getToken() !== null;
+    return this.isTokenValid();
+  }
+
+  isTokenValid(): boolean {
+    const token = this.getToken();
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const tokenPayload = JwtUtils.parseJwt(token);
+      const expirationDate = JwtUtils.fetchExpirationDate(tokenPayload);
+      return !JwtUtils.hasTokenExpired(expirationDate);
+    } catch (error) {
+      console.error('Error while trying to parse token! (EID: 202504181233)');
+      return false;
+    }
+  }
+
+  shouldRefreshToken(): boolean {
+    const token = this.getToken();
+    if (!token) return false;
+    
+    try {
+      const tokenPayload = JwtUtils.parseJwt(token);
+      const expirationDate = JwtUtils.fetchExpirationDate(tokenPayload);
+      return JwtUtils.isTokenCloseToExpiring(expirationDate);
+    } catch (error) {
+      return false;
+    }
+  }
+
+  performTokenRefresh(): void {
+    if (this.refreshInProgress) return;
+    
+    this.refreshInProgress = true;
+    this.refreshToken().subscribe({
+      next: () => {
+        this.showRefreshTokenMessage();
+        this.refreshInProgress = false;
+      },
+      error: () => {
+        this.refreshInProgress = false;
+      }
+    });
+  }
+
+  refreshToken(): Observable<AuthResponse> {
+    const token = this.getToken();
+    
+    if (!token) {
+      return throwError(() => new Error('No token available to refresh (EID:202504181330)'));
+    }
+    
+    return this.http.post<AuthResponse>(`${this.apiUrl}/api/auth/refresh`, {}, {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      })
+    }).pipe(
+      tap(response => {
+        if (response && response.token) {
+          this.setToken(response.token);
+        }
+      }),
+      catchError(error => {
+        if (error.status === 401) {
+          this.clearToken();
+        }
+        return throwError(() => error);
+      })
+    );
   }
 
   private updateLoginState(isLoggedIn: boolean): void {
     this.loginStateSubject.next(isLoggedIn);
+  }
+
+  private showRefreshTokenMessage() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Info',
+      detail: 'The login token has been successfully refreshed!',
+      life: 5000
+    });
   }
 }
 
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { MessageService } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private messageService: MessageService
+  ) {}
 
   canActivate(): boolean {
     if (this.authService.isLoggedIn()) {
       return true;
     } else {
+      this.authService.clearToken();
       this.router.navigate(['/login']);
+      this.showLogoutMessage();
       return false;
     }
+  }
+
+  private showLogoutMessage() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Info',
+      detail: 'You have been logged out because of the inactivity.',
+      life: 5000
+    });
   }
 }
