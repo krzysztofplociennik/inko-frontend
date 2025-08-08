e9568f9e7eef1072eb7714a297400e94cac9a4a9
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2025-08-08 14:34:56 +0200

    Fixed footer icons.

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.css'],
     encapsulation: ViewEncapsulation.Emulated,
+    imports: [
+        ButtonModule
+    ]
 })
 export class FooterComponent {}
